123de82915e84531054fdf7b73ced3ccbe4f858f
J-Tech-Foundation <52475982+J-Tech-Foundation@users.noreply.github.com>
2019-12-07 22:11:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 var fs = require("fs");
 var http = require("http");
 var url = require("url");
-function request(req, res) {
-  var stuff = url.parse(req.url, true);
-  if (stuff.pathname === "/"){
-    
-  }
+module.exports = {
 }
-var srv = http.createServer(request);
-
-srv.listen(8080);
